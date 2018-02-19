ad9566f1a42479c8d964958c57e11ad7bdddb30c
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2018-02-19 21:37:16 +0100

    Fix looper with async/await functions

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const app = express();
 
 const http = require("http").Server(app);
 const io = require("socket.io")(http, {
-	pingTimeout: 1000, // pingInterval: 100,
+	pingTimeout: 100, // pingInterval: 100,
 });
 const fs = require("fs");
 const path = require("path");
 
 const v4l2camera = require("v4l2camera");
 const cam = new v4l2camera.Camera("/dev/video1"); // /dev/video0 on rpi3
-
-const jimp = require("jimp");
+cam.start();
 
 app.use("/", express.static(path.join(__dirname, "stream")));
 app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
 
 const sockets = {};
+
 io.on("connection", socket => {
 	sockets[socket.id] = socket;
-	console.log(`Clients connected: ${Object.keys(sockets).length}`);
+	console.log(`\nClients connected: ${Object.keys(sockets).length}\n`);
 
 	socket.on("disconnect", () => {
 		delete sockets[socket.id];
@@ -38,53 +38,44 @@ http.listen(3000, () => {
 	console.log("listening on *:3000");
 });
 
-// function capture() {
-// 	console.log("start camera");
-// 	cam.start();
-// 	cam.capture(function loop() {
-// 		const frame = cam.frameRaw();
-// 		fs.createWriteStream("./stream/image_stream.jpg").end(Buffer(frame));
-// 		cam.capture(loop);
-// 	});
-// }
+function printProgress() {
+	process.stdout.clearLine();
+	process.stdout.cursorTo(0);
+	process.stdout.write(`○  ${(new Date() / 1e3) | 0} `);
 
-function capture() {
-	console.log("start camera");
-	cam.start();
-
-	function processImage() {
-		return new Promise(resolve => {
-			const frame = cam.frameRaw();
-			jimp
-				.read(Buffer(frame), function(err, image) {
-					image
-						.resize(640, 480)
-						.quality(50)
-						.greyscale()
-						.write("./stream/image_stream.jpg", () => {
-							resolve("save file");
-						});
-				})
-				.catch(() => resolve("err"));
-		});
-	}
-
-	async function loop() {
-		await processImage();
-		cam.capture(loop);
-	}
-	// cam.capture(loop);
-	loop();
+	setTimeout(() => {
+		process.stdout.clearLine();
+		process.stdout.cursorTo(0);
+		process.stdout.write(`●  ${(new Date() / 1e3) | 0} `);
+	}, 500);
 }
 
-capture();
+const getFrame = () =>
+	new Promise(resolve => {
+		setTimeout(() => {
+			cam.capture(() => resolve(cam.frameRaw()));
+		}, 1000);
+	});
+
+async function processImage() {
+	const frame = await getFrame();
+	fs.createWriteStream("./stream/image_stream.jpg").end(Buffer(frame));
+	processImage();
+	printProgress();
+}
+
+processImage();
 
 function startStreaming(io) {
 	fs.watchFile(
 		"./stream/image_stream.jpg",
-		{ persistent: true, interval: 500 },
+		{ persistent: false, interval: 100 },
 		(current, previous) => {
-			if (current.birthtime !== previous.birthtime) {
+			if (
+				current.birthtime !== previous.birthtime &&
+				current.size !== 0 &&
+				previous.size !== 0
+			) {
 				io.sockets.emit(
 					"liveStream",
 					`image_stream.jpg?_t=${(new Date() / 1e3) | 0}`
